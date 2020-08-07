6c1847dc35dd3eecf131816dac3b42619b756cc6
MrR3set <bairon.pazrobles@gmail.com>
2020-08-07 09:56:28 -0700

    ex4 done

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,17 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    a.sort()
+    pairs = {}
+    for nums in a:
+        if nums<0:
+            pairs[abs(nums)] = False
+        else:
+            if nums in pairs:
+                pairs[nums] = True
+
+
+    result=[key for key, value in pairs.items() if value == True]
 
     return result
 
