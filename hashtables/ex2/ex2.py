d60b9a64a39b8070b5d85a55c63686b9af881c73
MrR3set <bairon.pazrobles@gmail.com>
2020-08-07 09:46:51 -0700

    ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,25 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    routeHash = {}
+    route = [None] * length
+    val = routeHash["NONE"]
+    for ticket in tickets:
+        if ticket not in routeHash:
+            routeHash[ticket.source]=ticket.destination
+            
+    for i in range(0,length):
+        route[i] = val
+        val = routeHash[val]
 
     return route
+
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+print(reconstruct_trip(tickets, 3))
